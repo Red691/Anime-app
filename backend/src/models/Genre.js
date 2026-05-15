b09f5ddad27ba4c8f0e07572db46c50c5035de1d
Red691 <kumarnikhil05848@gmail.com>
2026-05-15 13:39:07 +0530

    Update Genre.js

--- backend/src/models/Genre.js
+++ backend/src/models/Genre.js
@@ -37,39 +37,3 @@ genreSchema.pre('save', function(next) {
 
 module.exports = mongoose.model('Genre', genreSchema);
 
-// src/models/Notification.js
-const notificationSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-    index: true
-  },
-  type: {
-    type: String,
-    enum: ['episode_release', 'announcement', 'reply', 'system', 'recommendation'],
-    required: true
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  message: String,
-  data: {
-    animeId: mongoose.Schema.Types.ObjectId,
-    episodeId: mongoose.Schema.Types.ObjectId,
-    url: String
-  },
-  isRead: {
-    type: Boolean,
-    default: false
-  },
-  sentAt: {
-    type: Date,
-    default: Date.now
-  }
-}, { timestamps: true });
-
-notificationSchema.index({ user: 1, isRead: 1, sentAt: -1 });
-
-module.exports = mongoose.model('Notification', notificationSchema);
